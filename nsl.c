3b65f7fa8521591c82073597ac29f533ff105420
user <user@ws1013402131.pm>
2024-10-17 12:41:47 +0500

    ahh

--- nsl.c
+++ nsl.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 FILE *li;
 FILE *lo; //Обьявляю файловые переменные
@@ -20,7 +19,14 @@ fscanf(li,"%d",&c2);
 fscanf(li,"%d",&c3);
 t=0;
 
-if ((c1==0)&(c2==0)&(c3==0)){fprintf(lo,"exit");return 0;}
+if ((c1==0)&(c2==0)&(c3==0))
+{
+    fprintf(lo,"exit");
+    fclose(lo);
+    fclose(li);
+    return(0);
+}
+
 if ((c1==c2) || (c1==c3) || (c2==c3))
 {
     fprintf(lo,"Error: Variables cannot be equal to each other\n"); //вывожу сообщение
