5ddf0ec70b5ea822cbcd16ab94cfeea1150d8670
user <user@ws1013402131.pm>
2024-10-17 13:38:44 +0500

    ahhh

--- nsl.c
+++ nsl.c
@@ -1,38 +1,43 @@
 #include <stdio.h>
 
-FILE *li;
+
 FILE *lo; //Обьявляю файловые переменные
 
 
 int main()
 {
-li=fopen("loginto.txt","r"); //присваиваю файловым переменным значения
-lo=fopen("logout.txt","w");
+lo=fopen("logout.txt","w"); //присваиваю файловым переменным значения
 int c1, c2, c3, max, sum, t;
 float n;
 do
 {
 do
 {
-fscanf(li,"%d",&c1); //читаю число из файла
-fscanf(li,"%d",&c2);
-fscanf(li,"%d",&c3);
+printf("you can stop the programm enter 0 0 0 \n");
+printf("programm works only with integer \n");
+printf("Enter c1 \n");
+scanf("%i", &c1);
+printf("Enter c2 \n");
+scanf("%i", &c2);
+printf("Enter c3 \n");
+scanf("%i", &c3);
+printf("\n");
 t=0;
 
 if ((c1==0)&(c2==0)&(c3==0))
 {
     fprintf(lo,"exit");
     fclose(lo);
-    fclose(li);
     return(0);
 }
-
+fprintf(lo,"Ввод пользователя: %i %i %i \n",c1 ,c2 ,c2);
 if ((c1==c2) || (c1==c3) || (c2==c3))
 {
-    fprintf(lo,"Error: Variables cannot be equal to each other\n"); //вывожу сообщение
+    fprintf(lo,"Error: Variables cannot be equal to each other\n \n"); //вывожу сообщение
     t=1;
 }
 }while(t==1);
+
 if(c1>c2)
 {
     if(c1>c3)
@@ -67,10 +72,10 @@ else
 {
     n=((float)sum/(float)max);
 }
-fprintf(lo,"%f\n", n); //вывожу значение
+printf("%f \n \n",n);
+fprintf(lo,"Вывод программы: %f\n \n", n); //вывожу значение
 }while((c1!=0) & (c2!=0) & (c3!=0));
 
 fclose(lo);
-fclose(li);
 return(0);
 }
